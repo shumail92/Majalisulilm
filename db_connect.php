e3986aaf594a6818910f4144108d6ac092175fd0
Shumail Mohy-ud-Din <shumailmohyuddin@gmail.com>
2015-11-25 22:39:37 +0500

    Initial dump for Majalis-ul-Ilm archive directory system

---
+++ db_connect.php
@@ -0,0 +1,11 @@
+<?php
+$mysqli = new mysqli("127.0.0.1", "root", "", "majalisilm");
+
+/* check connection */
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+//  echo "success";
+?>
---
+++ sidebar.php
@@ -0,0 +1,37 @@
+<!-- <span class="selected"></span> -->
+<div class="page-sidebar navbar-collapse collapse">
+   <!-- BEGIN SIDEBAR MENU -->
+   <ul class="page-sidebar-menu">
+      <li>
+         <!-- BEGIN SIDEBAR TOGGLER BUTTON -->
+         <div class="sidebar-toggler hidden-phone"></div>
+         <!-- BEGIN SIDEBAR TOGGLER BUTTON -->
+      </li>
+      <li>&nbsp;</li>
+      <li id="sidebar-dashboard" class="start ">
+         <a href="javascript;;">
+           <i class="icon-home"></i>
+           <span class="title">Dashboard</span>
+         </a>
+      </li>
+      <li id="sidebar-about" class="">
+         <a href="javascript:;">
+         <i class="icon-group"></i>
+         <span class="title">About</span>
+         <span class="arrow "></span>
+         </a>
+         <ul class="sub-menu">
+            <li id="sidebar-sub-about-series">
+               <a href="javascript;;">
+               The Series</a>
+            </li>
+            <li id="sidebar-sub-about-mqi">
+               <a href="javascript;;">
+              Minhaj-ul-Quran Intl.</a>
+            </li>
+        </ul>
+      </li>	  	  <li id="trigger_fullscreen" >         <a href="javascript:void(0)">           <i class="icon-move"></i>           <span class="title">Full Screen</span>         </a>      </li>	  
+      
+   </ul>
+   <!-- END SIDEBAR MENU -->
+</div>
